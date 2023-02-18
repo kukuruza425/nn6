0a086155cfa10d18200ec7116f2d89af5a45d21c
Vanclaah <reznared@gmail.com>
2023-02-18 17:01:28 +0300

    1 commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using System.Drawing;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mat pic = new Mat();
+            pic = CvInvoke.Imread(@"C:\Users\User\source\repos\ConsoleApp6\ConsoleApp6");
+            Mat gaussianBlur = new Mat();
+
+            CvInvoke.GaussianBlur(pic, gaussianBlur, new System.Drawing.Size(pic.Row, pic.Cols),6,6);
+
+            CvInvoke.Imshow("st n", pic);
+            CvInvoke.Imshow("bl n", gaussianBlur);
+            CvInvoke.WaitKey();
+        }
+    }
+}
